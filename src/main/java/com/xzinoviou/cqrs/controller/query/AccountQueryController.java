847068f8613b5471f65b2e12bf8c0b6aec6ca0cf
Xenofon Zinoviou <xzinoviou@gmail.com>
2021-06-13 11:24:57 +0300

    added AccountQueryController#getAccountById

--- src/main/java/com/xzinoviou/cqrs/controller/query/AccountQueryController.java
+++ src/main/java/com/xzinoviou/cqrs/controller/query/AccountQueryController.java
@@ -1,10 +1,29 @@
 package com.xzinoviou.cqrs.controller.query;
 
+import com.xzinoviou.cqrs.domain.jpa.Account;
+import com.xzinoviou.cqrs.service.query.AccountQueryService;
+import java.util.concurrent.CompletableFuture;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author : Xenofon Zinoviou
  */
 @RestController
+@RequestMapping("/accounts")
 public class AccountQueryController {
+
+  private final AccountQueryService accountQueryService;
+
+  public AccountQueryController(
+      AccountQueryService accountQueryService) {
+    this.accountQueryService = accountQueryService;
+  }
+
+  @GetMapping("/{accountId}")
+  public CompletableFuture<Account> getAccountById(@PathVariable String accountId) {
+    return accountQueryService.getAccountById(accountId);
+  }
 }
